78467b8639f4dc2dcb73d69019c6c1511c07fa31
Kubbany <2021170289@cis.asu.edu.eg>
2024-12-07 20:27:15 +0200

    add fire base to app

--- lib/features/cart/presentation/views/cart_view.dart
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/features/cart/presentation/views/widgets/cart_view_body.dart';
+import 'package:ecommerce_app/features/login&register/presentation/views/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class CartView extends StatelessWidget {
@@ -7,8 +8,72 @@ class CartView extends StatelessWidget {
   static const String id = kCartView;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CartViewBody(),
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(15),
+        child: CustomButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => const ConfirmOrder(),
+            );
+          },
+          title: "Submit Order",
+        ),
+      ),
+      body: const CartViewBody(),
+    );
+  }
+}
+
+class ConfirmOrder extends StatelessWidget {
+  const ConfirmOrder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      height: 600,
+      child: AlertDialog(
+        backgroundColor: kPrimaryColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
+          ),
+        ),
+        title: const Text(
+          "Order Preview",
+        ),
+        content: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 15,
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Order1",
+              ),
+              const Text(
+                "Order1",
+              ),
+              const Text(
+                "Order1",
+              ),
+              const Text(
+                "Order1",
+              ),
+              CustomButton(
+                onPressed: () {},
+                title: "Submit",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ import 'package:ecommerce_app/features/login&register/presentation/views/login_v
 import 'package:ecommerce_app/features/login&register/presentation/views/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
